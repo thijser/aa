fc1206987636798e1de33a0b255f54b012fb56a3
thijser <thijsboumans@online.nl>
2015-10-31 15:16:53 +0100

    premerge

--- exersise1/src/exercise1/ApproximateScheduler.java
+++ exersise1/src/exercise1/ApproximateScheduler.java
@@ -1,8 +1,19 @@
 package exercise1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  *Returns a approximately optimal schedule
  */
 public class ApproximateScheduler {
+    private schedule getSchedule(ArrayList<Integer> jobs, int time) {
+    	ArrayList<Integer> ljobs=jobs.clone()
+    	Sort();
+    	
+    	return null;
+    }
+    
     
 }
+
