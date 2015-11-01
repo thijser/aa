57ec968cd9f69a2e02525bf3c93a7c275712801d
Jorik Oostenbrink <jorik@oostenbrink.org>
2015-11-01 14:08:13 +0100

    Ready to test

--- exersise1/src/exercise1/LoopOver.java
+++ exersise1/src/exercise1/LoopOver.java
@@ -1,61 +1,47 @@
 package exercise1;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
-import com.sun.corba.se.impl.orbutil.closure.Future;
+import java.io.File;
 
 public class LoopOver {
-	
-	static int timeout=100000;
-	static double epsilon=1;
 
-	public static String location="/home/jorik/NetBeansProjects/Advanced Algorithms/tests";
+    static int timeout = 180000;
+    static double epsilon = 1;
+
+    public static String location = "C:\\Users\\Jorik\\Documents\\NetBeansProjects\\Advanced Algorithms\\tests";
+
     public static void main(String args[]) throws InterruptedException {
-    	
-    	  File dir = new File(location);
-    	  File[] directoryListing = dir.listFiles();
-    	  for(File f : directoryListing){
-    		  System.out.println();
-    		  System.out.print("RDD=;"+f.getName().substring(11, 14));
-    		  System.out.print(";TF=;"+f.getName().substring(18, 21));
-    		 algorithms.read_problem(f.getPath());
-   		  System.out.print(";size=;"+algorithms.num_jobs);
 
-    		
-    		  Thread t = new Thread(new executeAprox());
-    		  t.setPriority(Thread.MAX_PRIORITY);
-    		  t.start();
-    		  long start=System.currentTimeMillis();
-    		  t.join(timeout);
-    		  if(t.isAlive()){
-    			  t.stop();
-    			  t.join();
-    			  System.out.print("interupted;;;interupted;");
-    		  }
-    	  }
+        File dir = new File(location);
+        File[] directoryListing = dir.listFiles();
+        for (File f : directoryListing) {
+            System.out.println();
+            System.out.print("RDD=;" + f.getName().substring(11, 14));
+            System.out.print(";TF=;" + f.getName().substring(18, 21));
+            algorithms.read_problem(f.getPath());
+            System.out.print(";size=;" + algorithms.num_jobs);
+
+            Thread t = new Thread(new executeAprox());
+            t.setPriority(Thread.MAX_PRIORITY);
+            t.start();
+            t.join(timeout);
+            if (t.isAlive()) {
+                t.stop();
+                t.join();
+                System.out.print("interupted;;;interupted;");
+            }
+        }
     }
-    
-    
-    
-    
-}   
 
-class executeAprox implements Runnable{
+}
 
-	@Override
-	public void run() {
-		long time = System.currentTimeMillis();
-		schedule s = ApproximateScheduler.getSchedule(LoopOver.epsilon);
-		System.out.print(";duration=;"+(System.currentTimeMillis()-time));
-		System.out.println(";tardiness=;"+s.get_tardiness());
-	}
-	
+class executeAprox implements Runnable {
+
+    @Override
+    public void run() {
+        long time = System.currentTimeMillis();
+        schedule s = ApproximateScheduler.getSchedule(LoopOver.epsilon);
+        System.out.print(";duration=;" + (System.currentTimeMillis() - time));
+        System.out.println(";tardiness=;" + s.get_tardiness());
+    }
+
 }
